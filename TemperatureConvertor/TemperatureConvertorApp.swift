dbc42139fb57bb90dd71f32edce493672ba5dcb5
raszion23 <rastazion23@gmail.com>
2021-12-23 16:50:47 -0700

    Initial Commit

---
+++ TemperatureConvertor/TemperatureConvertorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TemperatureConvertorApp.swift
+//  TemperatureConvertor
+//
+//  Created by Raszion23 on 12/23/21.
+//
+
+import SwiftUI
+
+@main
+struct TemperatureConvertorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
